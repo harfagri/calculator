5b809fe22a5d9ac7d89798d28c51d41ff5c2077c
Josse Augusta <jaugusta@kvz.cl>
2020-09-01 14:36:40 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,136 @@
+var operator = null;
+var valueMemo = 0;
+
+function getContentClick(event){
+    const value = event.target.innerHTML;
+    filterAction(value)
+}
+
+const filterAction = value => {
+    value === "0" ? addNumberInput(0) : null;
+    value === "1" ? addNumberInput(1) : null;
+    value === "2" ? addNumberInput(2) : null;
+    value === "3" ? addNumberInput(3) : null;
+    value === "4" ? addNumberInput(4) : null;
+    value === "5" ? addNumberInput(5) : null;
+    value === "6" ? addNumberInput(6) : null;
+    value === "7" ? addNumberInput(7) : null;
+    value === "8" ? addNumberInput(8) : null;
+    value === "9" ? addNumberInput(9) : null;
+    value === "," ? addNumberInput(',') : null;
+
+    value === "X" ? setOperation('*') : null;
+    value === "/" ? setOperation('/') : null;
+    value === "+" ? setOperation('+') : null;
+    value === "-" ? setOperation('-') : null;
+    value === "%" ? setOperation('%') : null;
+    value === "+/-" ? setOperation('+/-') : null;
+    value === "=" ? calculation() : null;
+    value === "AC" ? resetOperation() : null;
+}
+
+function addNumberInput(value){
+    const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+    const inputValue = inputScreen.value;
+
+    if (inputValue === "0" && inputValue.length === 1 && value !== ","){
+        inputScreen.value = value;
+        return;
+    }
+
+    if (inputScreen.value === "" && value=== ","){
+        inputScreen.value = 0 + value;
+        return;
+    }
+
+    inputScreen.value = inputValue + value;
+}
+
+function setOperation(operator){
+    const inputScreenValue = document.getElementsByClassName('calculator__screen')[0].value;
+    this.operator = operator;
+
+    if(inputScreenValue != 0){
+        calculation();
+    }
+}
+
+function calculation(){
+    const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+    let valueOne = transformCommaToPoint(this.valueMemo);
+    let valueTwo =  transformCommaToPoint(inputScreen.value);
+    let total = 0;
+    console.log("v 1: "+ valueOne);
+    console.log("v 2: "+ valueTwo);
+    console.log("T: "+ total );
+
+    if(this.operator === "+" && inputScreen.value !== ""){
+        total = valueOne + valueTwo;
+    }
+
+
+    if(this.operator === "-" && inputScreen.value !== ""){
+        if(valueOne !== 0){
+        total = valueOne - valueTwo;
+        }else{
+            total = valueTwo;
+        }
+    }
+
+    if(this.operator === "*" && inputScreen.value !== ""){
+        if(valueOne !== 0){
+            total = valueOne * valueTwo;
+        }else{
+            total = valueTwo;
+        }   
+    }
+
+    if(this.operator === "/" && inputScreen.value !== ""){
+        if(valueOne !== 0){
+            total = valueOne / valueTwo;
+        }else{
+            total = valueTwo;
+        }
+    }
+
+    if(this.operator === "%" && inputScreen.value !== ""){
+        total = valueTwo / 100;
+    }
+
+    if(this.operator === "+/-" && inputScreen.value !== ""){
+        if(valueTwo > 0){
+            total = -valueTwo;
+        }
+    }
+// fix para presionar un valor solamente y el =
+    if(this.operator === null ){
+        total = valueTwo;
+    }
+
+    total = transformPointToComma(total);
+    this.valueMemo = total;
+    inputScreen.value = "";
+    inputScreen.placeholder = total;
+}
+
+function transformCommaToPoint(value){
+    if(typeof value !== "number"){
+        let resultTransform = value.replace(',','.');
+        return parseFloat(resultTransform)
+    }
+    return value;
+}
+
+function transformPointToComma(value){
+    let resultTransform = value.toString();
+    resultTransform = resultTransform.replace('.',',');
+        return resultTransform;
+
+}
+
+function resetOperation(){
+    const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+    inputScreen.value = 0;
+    this.valueMemo = 0;
+    this.operator = null;
+}
